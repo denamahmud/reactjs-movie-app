ca608d9d8c337d1158beb380a5b8c427554378ec
denamahmud <denamahmud>
2023-09-18 14:59:38 +0300

    Updated

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -59,7 +59,7 @@ useEffect(() => {
                           })
                         )
                       }
-                      <div className='text-white flex pt-10'>
+                      <div className='text-white flex pt-5'>
                           <p className='text-lightblue mr-4'>IMDB ID : </p> {details.imdb_id}
                       </div>
                       <div className='text-white flex'> <p className='text-lightblue mr-4'>Release date : </p> {details.release_date}</div>
